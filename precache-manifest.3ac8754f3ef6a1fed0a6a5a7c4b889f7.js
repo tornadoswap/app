ba1179853609557d61ed933d48815d37823bb2e2
p3titcr0c0 <petitcroco@gmail.com>
2021-05-17 15:22:20 +0200

    Updates

---
+++ precache-manifest.3ac8754f3ef6a1fed0a6a5a7c4b889f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3c11718214d194b0974c18954c5513",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "0500ef2483fe8994c77e",
+    "url": "/app/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "1266935d95f08bec3b18",
+    "url": "/app/static/js/2.bde0b825.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/app/static/js/2.bde0b825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0500ef2483fe8994c77e",
+    "url": "/app/static/js/main.b16e525c.chunk.js"
+  },
+  {
+    "revision": "fb683d993c694aeb9312",
+    "url": "/app/static/js/runtime-main.95af9303.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/app/static/media/logo.5d5d9eef.svg"
+  }
+]);
